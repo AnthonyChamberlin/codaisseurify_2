68e6489d3bc33dfb3de7b21569bc809db6944eec
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-10-18 23:00:44 +0200

    Change Actions in Song and Artist Controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,17 +1,26 @@
 class ArtistsController < ApplicationController
-  before_action :find_artist, only: [:show, :destroy]
+  before_action :find_artist, only: [:show, :create, :destroy]
 
-  def index
-    @artists = Artist.all
-  end
+    def index
+      @artists = Artist.all
+    end
 
-  def show
-  end
+    def show
+      @songs = @artist.songs
+    end
 
-  private
+    def create
+      @songs = @artist.songs
+    end
 
-  def find_artist
-  @artist = Artist.find(params[:id])
-  end
+    def destroy
+      @artist.destroy
+      redirect_to artists_path
+    end
 
+    private
+
+    def find_artist
+      @artist = Artist.find(params[:id])
+    end
 end
